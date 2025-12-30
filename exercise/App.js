e730cb276f1d5d76ef4f15e32a0bd26c4794ca96
deshmukh-somesh <deshmukhsomesh7777@gmail.com>
2025-12-30 21:17:37 +0530

    exercise 1 completed

---
+++ exercise/App.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+// import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+
+export default function App() {
+  return <ViewImageScreen />;
+}
+
+const styles = StyleSheet.create({});
