e367317cfc63bdfc9b47ec5621a176fd4ae3cacd
deshmukh-somesh <deshmukhsomesh7777@gmail.com>
2025-12-30 17:39:14 +0530

    added flex

---
+++ flexbox/App.js
@@ -0,0 +1,36 @@
+import { View } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          // flex: 2,
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
+        }}
+      />
+    </View>
+  );
+}
